c7d15e78c60b2aa09b6181c28654011394f3063d
minhquang2k4 <minhwang1708@gmail.com>
2024-08-30 23:53:13 +0700

    init redux toolkit

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Button } from '@mui/material'
-// import DeleteIcon from '@mui/icons-material/Delete'
+// import DeleteIcon from '@mui/icons-material/Delete';
+
+// action 
+import { LOGIN, LOGOUT } from './redux/slices/authSlice'
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user) // Lấy giá trị từ Redux store
+
+  const handleClick = () => {
+    console.log('click')
+    dispatch(LOGIN({ name: 'John Doe' }))
+  }
+
+  const logout = () => {
+    dispatch(LOGOUT())
+  }
 
   return (
     <>
-      <Button variant="contained" color="secondary">
+      <Button variant="contained" color="secondary" onClick={handleClick}>
         Primary Button
+      </Button>
+      {user && <div>User: {user.name}</div>} {/* Hiển thị giá trị từ Redux store */}
+      <Button variant="contained" color="secondary" onClick={logout}>
+        Logout
       </Button>
     </>
   )
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import CssBaseline from '@mui/material/CssBaseline' // cssbaseline is a global css reset
 import { ThemeProvider } from '@mui/material/styles'
 
+import store from './redux/store.js'
 import App from './App.jsx'
 import theme from './theme.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 )
